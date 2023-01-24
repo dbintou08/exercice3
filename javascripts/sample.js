4d7d7213cda5594b33f00ca9e3df03c47894d282
dbintou08 <dbintou008@gmail.com>
2023-01-24 18:23:29 +0000

    testsoumis

---
+++ javascripts/sample.js
@@ -0,0 +1,9 @@
+let firstName = "Taro";
+
+function greeting() {
+  let familyName = "DIVE";
+  console.log(familyName,firstName); //Appeler une variable définie en dehors d'une fonction
+}
+
+greeting();
+console.log(familyName); 
